045fcde3378b5861c18e107d0d6c08b8c0454e92
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2025-02-12 20:58:08 -0300

    update

--- src/api/api.ts
+++ src/api/api.ts
@@ -21,6 +21,8 @@ api.interceptors.request.use(
     if (!isAccessTokenValid()) {
       // Se o accessToken não é válido, redireciona para o login
       window.location.href = "/login"; 
+      return Promise.reject(new Error("Token inválido. Redirecionando para login."));
+
     } else {
       // Se o token for válido, inclui no cabeçalho da requisição
       config.headers["Authorization"] = `Bearer ${getAccessToken()}`;
@@ -31,6 +33,18 @@ api.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      logout(); // Remove tokens inválidos
+      window.location.href = "/login"; // Redireciona para login
+    }
+    return Promise.reject(error);
+  }
+);
+
 
 
 export default api;
